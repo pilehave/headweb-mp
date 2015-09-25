212fe073e23ef86a0d84251bde733b59bc7e1684
pilehave <mj@cm3.dk>
2015-09-25 11:21:50 +0200

    Moved fram Google Code to GitHub.

---
+++ hw/HeadWebConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MediaPortal.Configuration;
+using System.Reflection;
+
+namespace HeadWeb
+{
+  public partial class HeadWebConfig : Form
+  {
+    public HeadWebConfig()
+    {
+      InitializeComponent();
+    }
+
+    private void frmHeadWebLoadSettings(object sender, EventArgs e)
+    {
+      using (MediaPortal.Profile.Settings xmlreader = new MediaPortal.Profile.Settings(Config.GetFile(Config.Dir.Config, "HeadWeb.xml")))
+      {
+        inputUsername.Text = xmlreader.GetValueAsString("Settings", "Username", "");
+        inputPassword.Text = xmlreader.GetValueAsString("Settings", "Password", "");
+        inputPincode.Text = xmlreader.GetValueAsString("Settings", "PINCode", "");
+        if (xmlreader.GetValueAsString("Settings", "VideoQuality", "").Length <= 0)
+        {
+          videoQuality.SelectedIndex = 0;
+        }
+        else
+        {
+          videoQuality.SelectedIndex = videoQuality.Items.IndexOf(xmlreader.GetValueAsString("Settings", "VideoQuality", ""));
+        }
+        if (xmlreader.GetValueAsInt("Settings", "AutoLogin", 0) != 0) checkboxLogin.Checked = true;
+        if (xmlreader.GetValueAsInt("Settings", "PinApprovePurchase", 0) != 0) checkboxPin.Checked = true;
+        if (xmlreader.GetValueAsInt("Settings", "DisableAdultMovies", 0) != 0) checkboxAdult.Checked = true;
+        if (xmlreader.GetValueAsInt("Settings", "UseOrginalName", 0) != 0) checkboxOrginalName.Checked = true;
+      }
+    }
+
+    // Save settings to file
+    private void btSave_Click(object sender, EventArgs e)
+    {
+      using (MediaPortal.Profile.Settings xmlwriter = new MediaPortal.Profile.Settings(Config.GetFile(Config.Dir.Config, "HeadWeb.xml")))
+      {
+        xmlwriter.SetValue("Settings", "Username", inputUsername.Text);
+        xmlwriter.SetValue("Settings", "Password", inputPassword.Text);
+        xmlwriter.SetValue("Settings", "PINCode", inputPincode.Text);
+        xmlwriter.SetValue("Settings", "VideoQuality", videoQuality.Text);
+        xmlwriter.SetValue("Settings", "AutoLogin", checkboxLogin.Checked ? 1 : 0);
+        xmlwriter.SetValue("Settings", "PinApprovePurchase", checkboxPin.Checked ? 1 : 0);
+        xmlwriter.SetValue("Settings", "DisableAdultMovies", checkboxAdult.Checked ? 1 : 0);
+        xmlwriter.SetValue("Settings", "UseOrginalName", checkboxOrginalName.Checked ? 1 : 0);
+        this.Close();
+      }
+    }
+
+    private void btCancel_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+  }
+}
---
+++ hw/HeadWebSettings.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Net;
+
+namespace HeadWeb
+{
+
+  public class HeadWebSettings
+  {
+    public string UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; de; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3";
+    public bool isPurchasePinProtected = true; // enable pin by default -> purchase protection
+    public string ThumbsDir;
+    public int CacheTimeout = 30; // minutes
+    public CultureInfo Locale;
+
+    #region Singleton
+    private static HeadWebSettings _Instance = null;
+    public static HeadWebSettings Instance
+    {
+      get
+      {
+        if (_Instance == null) _Instance = new HeadWebSettings();
+        return _Instance;
+      }
+    }
+    #endregion
+
+    static string movietitle = string.Empty;
+    static string username = string.Empty;
+    static string password = string.Empty;
+    static string firstname = string.Empty;
+    static string lastname = string.Empty;
+    static string pincode = string.Empty;
+    static string videoquality = string.Empty;
+    static bool autologin = false;
+    static bool pinapprovepurchase = false;
+    static bool disableadultmovies = false;
+    static bool useoriginalname = false;
+    static string balance = string.Empty;
+    static bool loggedin = false;
+    static CookieContainer cc = new CookieContainer();
+    List<int> favoritesList = new List<int>();
+    string searchHistory = string.Empty;
+
+    public string s_movietitle
+    {
+      get { return movietitle; }
+      set { movietitle = value; }
+    }
+
+    public string s_username
+    {
+      get { return username; }
+      set { username = value; }
+    }
+
+    public string s_password
+    {
+      get { return password; }
+      set { password = value; }
+    }
+
+    public string s_firstname
+    {
+      get { return firstname; }
+      set { firstname = value; }
+    }
+
+    public string s_lastname
+    {
+      get { return lastname; }
+      set { lastname = value; }
+    }
+
+    public string s_pincode
+    {
+      get { return pincode; }
+      set { pincode = value; }
+    }
+
+    public string s_video_quality
+    {
+      get { return videoquality; }
+      set { videoquality = value; }
+    }
+
+    public bool s_autologin
+    {
+      get { return autologin; }
+      set { autologin = value; }
+    }
+
+    public bool s_pinapprovepurchase
+    {
+      get { return pinapprovepurchase; }
+      set { pinapprovepurchase = value; }
+    }
+
+    public bool s_disableadultmovies
+    {
+      get { return disableadultmovies; }
+      set { disableadultmovies = value; }
+    }
+
+    public bool s_useoriginalname
+    {
+      get { return useoriginalname; }
+      set { useoriginalname = value; }
+    }
+
+    public string s_balance
+    {
+      get { return balance; }
+      set { balance = value; }
+    }
+
+    public bool s_loggedin
+    {
+      get { return loggedin; }
+      set { loggedin = value; }
+    }
+
+    public CookieContainer s_cc
+    {
+      get { return cc; }
+      set { cc = value; }
+    }
+
+    public List<int> s_favorit
+    {
+      get { return favoritesList; }
+      set { favoritesList = value; }
+    }
+
+    public string s_search_history
+    {
+      get { return searchHistory; }
+      set { searchHistory = value; }
+    }
+
+    private HeadWebSettings()
+    {
+      Locale = CultureInfo.CurrentUICulture;
+    }
+  }
+}
